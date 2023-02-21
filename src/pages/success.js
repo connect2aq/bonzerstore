be7fe3d8359b0f1de61ab44c4a6f58546b508cda
Abdul Qadir Khan <connect2aq@gmail.com>
2023-02-21 18:00:57 +0500

    3rd

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,32 +1,6 @@
-import Header from '../components/Header';
-import { CheckCircleIcon } from '@heroicons/react/solid';
-import { useRouter } from 'next/router';
-
 function Success() {
   const router = useRouter();
-  return (
-    <div className="bg-gray-100 h-screen">
-      <Header />
-
-      <main className="max-w-screen-lg mx-auto">
-        <div className="flex flex-col p-10 bg-white">
-          <div className="flex items-center space-x-2 mb-5">
-            <CheckCircleIcon className="text-green-500 h-10" />
-            <h1 className="text-3xl">
-              Thank you, your order has been confirmed!
-            </h1>
-          </div>
-          <p>Thank you.</p>
-          <button
-            onClick={() => router.push('/orders')}
-            className="button mt-8"
-          >
-            Go to my orders
-          </button>
-        </div>
-      </main>
-    </div>
-  );
+  return <h1>success</h1>;
 }
 
 export default Success;
