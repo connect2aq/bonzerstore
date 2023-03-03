3321a80e819b6986d3313aa0c4e292876cde86f7
Abdul Qadir Khan <connect2aq@gmail.com>
2023-03-03 23:07:41 +0500

    fix github issue

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Image from 'next/image';
-import { InformationCircleIcon } from '@heroicons/react/outline';
 
 function TopBar() {
   const description = `eCommerce with NEXT JS. Log In and Log Out authentication Flow with NextAuth. Add to Basket Functionality using Redux toolkit. Basket page where you can add/remove products and change their quantities. Complete Stripe Payments Checkout Flow for all the items in basket. Used Tailwind CSS to built a beautiful responsive Amazon Clone. 
